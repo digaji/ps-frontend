1b4258eebf7d5d6b90b62139b42aa344304827f5
Monique <monique.senjaya@binus.ac.id>
2022-10-21 23:47:03 +0700

    nav bar done

--- pages/project.tsx
+++ pages/project.tsx
@@ -1,13 +1,14 @@
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Link from '../src/Link';
-import styles from "../styles/Home.module.css";
 import ProTip from '../src/ProTip';
 import Copyright from '../src/Copyright';
 import Navbar from '../components/navbar/navbar';
 
-export default function Home() {
+
+export default function Project() {
   return (
     <>
     <Navbar/>
@@ -21,17 +22,7 @@ export default function Home() {
           alignItems: 'center',
         }}
       >
-
-
-        <Typography variant="h4" component="h1" gutterBottom>
-          MUI v5 + Next.js with TypeScript example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <Link href="/profile" color="secondary">
-          Go to the Profile page
-        </Link>
+        PROJECT PAGE
         <ProTip />
         <Copyright />
       </Box>
