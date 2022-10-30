a0b4067b90fb2be89c982fb216d0b8195b41663e
Ardelia <109872789+moonchild3e@users.noreply.github.com>
2022-10-30 23:43:04 +0700

    change colour and re-size

--- front-end/components/Navbar.jsx
+++ front-end/components/Navbar.jsx
@@ -6,8 +6,8 @@ import logo from '../src/site-logo.png'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
-  const [color, setColor] = useState('transparent');
-  const [textColor, setTextColor] = useState('black');
+  const [color, setColor] = useState('#2c6698');
+  const [textColor, setTextColor] = useState('white');
 
   const handleNav = () => {
     setNav(!nav);
@@ -16,10 +16,10 @@ const Navbar = () => {
   useEffect(() => {
     const changeColor = () => {
       if (window.scrollY >= 90) {
-        setColor('#ffffff');
+        setColor('#2c6698');
         setTextColor('#000000');
       } else {
-        setColor('transparent');
+        setColor('#2c6698');
         setTextColor('#ffffff');
       }
     };
@@ -35,20 +35,19 @@ const Navbar = () => {
         <Link href='/'>
         <Image
         src={logo}
-        alt="Picture of the author"
       />
         </Link>
         <ul style={{ color: `${textColor}` }} className='hidden sm:flex'>
-          <li className='p-4'>
+          <li className='p-4 font-bold'>
             <Link href='/'>Home</Link>
           </li>
-          <li className='p-4'>
+          <li className='p-4 font-bold'>
             <Link href='/project'>Projects</Link>
           </li>
-          <li className='p-4'>
+          <li className='p-4 font-bold'>
             <Link href='/publish'>Publish</Link>
           </li>
-          <li className='p-4'>
+          <li className='p-4 font-bold'>
             <Link href='/profile'>Profile</Link>
           </li>
         </ul>
@@ -70,7 +69,7 @@ const Navbar = () => {
           }
         >
           <ul>
-            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
+            <li onClick={handleNav} className='p-4 font-normal hover:text-gray-500'>
               <Link href='/'>Home</Link>
             </li>
             <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
