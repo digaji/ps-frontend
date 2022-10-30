2d0fb9f5274b82fbf7297061d41da6070765fdd1
Ardelia <109872789+moonchild3e@users.noreply.github.com>
2022-10-30 22:02:03 +0700

    refactor js and add navbar

---
+++ front-end/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+   <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
+
+   <div/>
+   <div>
+    <h2></h2>
+    <p></p>
+    <button></button>
+
+
+   </div>
+   </div>
+
+  );
+};
+
+export default Hero;
--- src/components/navbar.tsx
+++
@@ -1,5 +0,0 @@
-export default function Navbar(): JSX.Element {
-  return (
-    <nav className="border-gray-200 bg-transparent px-5 py-3 font-medium"></nav>
-  );
-}
