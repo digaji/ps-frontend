68c6ef641f019b9e7792bcf2094ffcb428c6fe70
Sleepy <sensen.edsel@gmail.com>
2022-11-05 18:55:22 +0700

    update the somthg
    1260px to 100 % something :)

--- front-end/components/Navbar.jsx
+++ front-end/components/Navbar.jsx
@@ -1,13 +1,13 @@
-import Link from 'next/link';
-import React, { useState, useEffect } from 'react';
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
-import Image from 'next/image'
-import logo from '../src/site-logo.png'
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import Image from "next/image";
+import logo from "../src/site-logo.png";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
-  const [color, setColor] = useState('#2c6698');
-  const [textColor, setTextColor] = useState('white');
+  const [color, setColor] = useState("#2c6698");
+  const [textColor, setTextColor] = useState("white");
 
   const handleNav = () => {
     setNav(!nav);
@@ -16,44 +16,42 @@ const Navbar = () => {
   useEffect(() => {
     const changeColor = () => {
       if (window.scrollY >= 90) {
-        setColor('#2c6698');
-        setTextColor('#000000');
+        setColor("#2c6698");
+        setTextColor("#000000");
       } else {
-        setColor('#2c6698');
-        setTextColor('#ffffff');
+        setColor("#2c6698");
+        setTextColor("#ffffff");
       }
     };
-    window.addEventListener('scroll', changeColor);
+    window.addEventListener("scroll", changeColor);
   }, []);
 
   return (
     <div
       style={{ backgroundColor: `${color}` }}
-      className='absolute left-0 top-0 w-full z-10 ease-in duration-300'
+      className="absolute left-0 top-0 w-full z-10 ease-in duration-300"
     >
-      <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-black'>
-        <Link href='/'>
-        <Image
-        src={logo}
-      />
+      <div className="max-w-[100%] m-auto flex justify-between items-center p-4 text-black">
+        <Link href="/">
+          <Image src={logo} />
         </Link>
-        <ul style={{ color: `${textColor}` }} className='hidden sm:flex'>
-          <li className='p-4 font-bold text-[22px]'>
-            <Link href='/'>Home</Link>
+        <ul style={{ color: `${textColor}` }} className="hidden sm:flex">
+          <li className="p-4 font-bold text-[22px]">
+            <Link href="/">Home</Link>
           </li>
-          <li className='p-4 font-bold text-[22px]'>
-            <Link href='/project'>Projects</Link>
+          <li className="p-4 font-bold text-[22px]">
+            <Link href="/project">Projects</Link>
           </li>
-          <li className='p-4 font-bold text-[22px]'>
-            <Link href='/publish'>Publish</Link>
+          <li className="p-4 font-bold text-[22px]">
+            <Link href="/publish">Publish</Link>
           </li>
-          <li className='p-4 font-bold text-[22px]'>
-            <Link href='/profile'>Profile</Link>
+          <li className="p-4 font-bold text-[22px]">
+            <Link href="/profile">Profile</Link>
           </li>
         </ul>
 
         {/* Mobile Button */}
-        <div onClick={handleNav} className='block sm:hidden z-10'>
+        <div onClick={handleNav} className="block sm:hidden z-10">
           {nav ? (
             <AiOutlineClose size={20} style={{ color: `${textColor}` }} />
           ) : (
@@ -64,22 +62,34 @@ const Navbar = () => {
         <div
           className={
             nav
-              ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-[#2c6698] text-white text-center ease-in duration-300'
-              : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-[#2c6698] text-white text-center ease-in duration-300'
+              ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-[#2c6698] text-white text-center ease-in duration-300"
+              : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-[#2c6698] text-white text-center ease-in duration-300"
           }
         >
           <ul>
-            <li onClick={handleNav} className='p-4 font-bold hover:text-gray-500'>
-              <Link href='/'>Home</Link>
+            <li
+              onClick={handleNav}
+              className="p-4 font-bold hover:text-gray-500"
+            >
+              <Link href="/">Home</Link>
             </li>
-            <li onClick={handleNav} className='p-4 font-bold hover:text-gray-500'>
-              <Link href='/#gallery'>Projects</Link>
+            <li
+              onClick={handleNav}
+              className="p-4 font-bold hover:text-gray-500"
+            >
+              <Link href="/#gallery">Projects</Link>
             </li>
-            <li onClick={handleNav} className='p-4 font-bold hover:text-gray-500'>
-              <Link href='/work'>Publish</Link>
+            <li
+              onClick={handleNav}
+              className="p-4 font-bold hover:text-gray-500"
+            >
+              <Link href="/work">Publish</Link>
             </li>
-            <li onClick={handleNav} className='p-4 font-bold hover:text-gray-500'>
-              <Link href='/contact'>Profile</Link>
+            <li
+              onClick={handleNav}
+              className="p-4 font-bold hover:text-gray-500"
+            >
+              <Link href="/contact">Profile</Link>
             </li>
           </ul>
         </div>
