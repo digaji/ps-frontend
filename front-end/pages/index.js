cad306130947cfa05503ca11a611a5e1eb56348b
Jason J. W <71363607+digaji@users.noreply.github.com>
2022-11-27 13:12:02 +0700

    Deleted Material UI from dependencies

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import Hero from "../components/Hero";
-import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar";
 import ProjectCard from "../components/ProjectCard";
 import ProfilePage from "../components/ProfilePage";
