776bdd2d32d6cfe3e96ed6a0f16424cbbc95c850
Bryn Ghiffar <bryn.ghiffar@gmail.com>
2022-12-05 01:49:03 +0700

    added some data fetching to profile page

--- front-end/components/ProfilePage.jsx
+++ front-end/components/ProfilePage.jsx
@@ -3,6 +3,59 @@ import { useMsal } from "@azure/msal-react";
 import { loginRequest } from "../azureAuth.config";
 import { callMsGraph } from "../azureGraph.config";
 
+const HOST = "http://localhost:8000";
+
+async function createUser(provider, providerId, useremail, username) {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  console.log(useremail);
+  var raw = JSON.stringify({
+    "provider_id": providerId,
+    "provider": provider,
+    "name": username,
+    "email": useremail,
+    "profile_pic": "123",
+    "description": `Hi, my name is ${username}. Very nice to meet you.`,
+    "graduation_year": "2025",
+    "current_semester": "3"
+  });
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  const res = await fetch(`${HOST}/service/user/v1`, requestOptions)
+    .then(response => response.text())
+    .then(result => JSON.parse(result))
+    .catch(error => console.log(error));
+  return res;
+}
+
+async function findCreateUserByProviderId(provider, providerId, useremail, username) {
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+
+  const res = await fetch(`${HOST}/service/user/v1/${provider}/${providerId}`, requestOptions)
+    .then(response => response.text())
+    .then(result => JSON.parse(result))
+    .catch(error => JSON.parse(error.text()));
+  
+  const USER_PROVIDER_ID_NOT_EXISTS = "USER_PROVIDER_ID_NOT_EXISTS";
+  if (res.message.includes(USER_PROVIDER_ID_NOT_EXISTS)) {
+    console.log("USER DOES NOT EXIST")
+    const res = createUser(provider, providerId, useremail, username);
+    console.log(res);
+    return res.user;
+  } else {
+    return res.user;
+  }
+}
+
 function ProfilePage() {
   const [file, setFile] = useState();
   function handleChange(e) {
@@ -13,19 +66,43 @@ function ProfilePage() {
   const {instance, accounts} = useMsal();
   const [graphData, setGraphData] = useState(null);
 
+  const [userData, setUserData] = useState({
+    "provider_id": "1",
+    "provider": "MSFT",
+    "name": "",
+    "email": "",
+    "profile_pic": "",
+    "description": "",
+    "graduation_year": "",
+    "current_semester": ""
+  });
+
+
   function RequestProfileData() {
     // Silently acquires an access token which is then attached to a request for MS Graph data
     instance.acquireTokenSilent({
         ...loginRequest,
         account: accounts[0]
     }).then((response) => {
-        callMsGraph(response.accessToken).then(response => setGraphData(response));
+        callMsGraph(response.accessToken).then(response => {
+          const providerId = response.id;
+          const provider = "MSFT";
+          const useremail = response.mail;
+          const username = response.displayName;
+          console.log(response);
+          setGraphData(response);
+          return findCreateUserByProviderId(provider, providerId, useremail, username);
+        }).then(user => {
+          console.log(user);
+          setUserData(_ => user);
+        })
+        ;
     }).catch(error => console.log(error));
   }
 
   useEffect(() => {
     RequestProfileData();
-  })
+  }, []);
 
   return (
     <div className="grid grid-cols-4 gap-4">
@@ -49,12 +126,10 @@ function ProfilePage() {
       <div className="col-span-2 font-Inter text-black text-[40px] md:text-[40px] font-[700] pt-20 ">
         { graphData ? <div> {graphData.givenName} {graphData.surname} </div> : <div> John Doe </div> }
       </div>
-      <div className="col-span-2 pt-0"> Semester 5, 2025</div>
+      <div className="col-span-2 pt-0"> Semester {userData.current_semester}, {userData.graduation_year}</div>
       <div className="col-span-2 box-content h-20 w-90 p-4 bg-[#EEEE] m4 pt">
         <ul className="list-none">
-          <li>Hello Everyone, my name is John Doe. I like programming and cooking.</li>
-          <li>Nice to meet you.</li>
-          <li>Github: http://</li>
+          <li>{userData.description}</li>
         </ul>
       </div>
     </div>
